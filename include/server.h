ed80f022eda3d627e818f8601485639df8f827b6
asqel <91128487+asqel@users.noreply.github.com>
2025-11-22 20:07:59 +0100

    checkname

--- include/server.h
+++ include/server.h
@@ -76,6 +76,6 @@ void srv_send(client_t *clt, uint32_t opcode, void *data, uint16_t len);
 void srv_send_err(client_t *clt, uint32_t err);
 int srv_create_account(client_t *clt, void *data, uint16_t len, server_t *srv);
 int srv_auth_account(client_t *clt, void *data, uint16_t len, server_t *srv);
-
+int srv_is_name_valid(char *name);
 
 #endif
--- src/server/room.c
+++ src/server/room.c
@@ -12,6 +12,10 @@ static int get_infos(void *data, uint16_t len, char **name, char **hash, client_
 		srv_send_err(clt, OPC_ERR_INVALID_DATA);
 		return 1;
 	}
+	if (!srv_is_name_valid(data)) {
+		srv_send_err(clt, OPC_ERR_NAME_INVALID);
+		return 1;	
+	}
 	*name = (char *)data;
 	*hash = (char *)data + name_len + 1;
 	return 0;
@@ -22,6 +26,6 @@ int srv_create_room(client_t *clt, void *data, uint16_t len, server_t *srv) {
 	char *hash = NULL;
 	if (get_infos(data, len, &name, &hash, clt)
 		return 0;
-		
+			
 	return 0;
 }
