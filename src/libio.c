48d42340ef5bb3b8288c42fa2e1198833043952f
asqel <axel.lleres@gmail.com>
2026-05-09 01:22:17 +0200

    a

--- src/libio.c
+++ src/libio.c
@@ -1,5 +1,10 @@
 #include "libio.h"
 
+static int g_fd = -1;
+static buffer_t g_received = {0};
+static buffer_t g_to_send = {0};
+static thrd_t loop_thread = {0};
+
 int libio_init(char *ip, int do_stdin) {
 	
 	return 0;
